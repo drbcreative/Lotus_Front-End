df4d59b5bc74243301d5229de426326f50845eb3
Danny <dannybcreative@gmail.com>
2019-09-24 11:42:11 -0700

    Art Dept. changes complete, ready for WP

--- js/app.js
+++ js/app.js
@@ -1,60 +1,24 @@
-/* Nav Location and Re-Location */
-const navBar = document.querySelector('.navbar-wrapper'),
-  headerContainer = document.querySelector('#header-container'),
-  mobileHeader = document.querySelector('#mobile-header');
+/* Menu Background */
+const navBar = document.querySelector('.nav-wrapper'),
+  siteLogo = document.querySelector('.nav-logo'),
+  bsNavbar = document.querySelector('.navbar');
 
-/* Event Listeners */
-document.addEventListener('DOMContentLoaded', () => {
-  const winWidth = window.innerWidth;
-  if (winWidth >= 992) {
-    desktopNav();
-  }
-});
-
-window.addEventListener('resize', () => {
-  const winWidth = window.innerWidth;
-  if (winWidth >= 992) {
-    desktopNav();
-  } else if (winWidth < 992) {
-    mobileNav();
-  }
-});
-
-/* Functions */
-const desktopNav = () => {
-  headerContainer.appendChild(navBar);
-}
-const mobileNav = () => {
-  mobileHeader.appendChild(navBar);
-}
-
-/* Animation */
-const mindWrapper = document.querySelector('.mind'),
-  mindBox = mindWrapper.querySelector('.section-info'),
-  productWrapper = document.querySelector('.product-wrapper'),
-  productBox = productWrapper.querySelectorAll('.product-box'),
-  winHeight = window.innerHeight;
+// window.addEventListener('resize', () => {
+//   if (window.innerWidth >= 992 && window.scrollY > 50) {
+//     headerMain.classList.remove('show');
+//   }
+// });
 
 window.addEventListener('scroll', () => {
-
-  if (mindWrapper.getBoundingClientRect().top < winHeight / 6) {
-    mindBox.classList.add('show');
+  if (window.scrollY > 30) {
+    navBar.classList.add('show');
+    siteLogo.classList.add('show');
+    bsNavbar.classList.add('show');
+  } else if (window.innerWidth >= 1200 && window.scrollY > 30) {
+    bsNavbar.classList.add('show');
   } else {
-    mindBox.classList.remove('show');
-  }
-
-
-  if (productWrapper.getBoundingClientRect().top < winHeight / 3) {
-    productBox.forEach((staff, i) => {
-      setTimeout(() => {
-        staff.classList.add("show");
-      }, i * 350);
-    });
-  } else {
-    productBox.forEach((staff, i) => {
-      setTimeout(() => {
-        staff.classList.remove("show");
-      }, i * 350);
-    });
+    navBar.classList.remove('show');
+    siteLogo.classList.remove('show');
+    bsNavbar.classList.remove('show');
   }
 });
